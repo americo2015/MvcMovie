4dab57ae17b690687462f37c0bcc43cc6486c329
Americo <Americo@DESKTOP-LBAHEL4>
2017-12-20 12:33:55 +0100

    Added search and filter

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
@@ -9,9 +6,12 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
